bf9aaf920bb1caefadb87c2d0ab364970e0cc7b0
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2023-06-01 11:17:48 +0530

    Create decimal.c

---
+++ decimal.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(){
+  // decimal|integer
+  int num1;
+  num1 = 10;
+  // or
+  int num2 = 10;
+  printf("%d %d", num1, num2) // d = decimal we can even use i = integer
+  // output: 10 10
+   
